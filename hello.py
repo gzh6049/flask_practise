f0e422d7a3f5417dab876eafe5a84510281c34f1
gzh <a15839986049@163.com>
2019-07-12 01:08:03 +0800

    重定向和用户会话

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template,session,redirect,url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -27,11 +27,13 @@ def internal_server_error(e):
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    name=None
+    # name=None
     form=NameForm()
     if form.validate_on_submit():
-        name=form.name.data
-        form.name.data=''
-    return render_template('index.html',form=form,name=name)
+        # name=form.name.data
+        # form.name.data=''
+        session['name']=form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form,name=session.get('name'))
 
 
