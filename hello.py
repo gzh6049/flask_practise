3529fb65422d28ec4573b966e6d85d722611a5d6
gzh <a15839986049@163.com>
2019-07-15 23:38:56 +0800

    配置数据库和定义Role和User模型

--- hello.py
+++ hello.py
@@ -4,16 +4,43 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired 
+
+import os
+from flask_sqlalchemy import SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE']=\
+    'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
 
 class NameForm(FlaskForm):
     name=StringField('What is your name?',validators=[DataRequired()])
     submit=SubmitField('Submit')
+
+class Role(db.Model):
+    __tablename__='roles'
+    id = db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(64),unique=True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(64),unique=True,index=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+        
 
 @app.errorhandler(404)
 def page_not_found(e):
