633ec189798aa80f91294c56a8da86bdfaa554cd
gzh <a15839986049@163.com>
2019-07-18 23:48:58 +0800

    修正了定义Role和User的模型

--- hello.py
+++ hello.py
@@ -8,10 +8,11 @@ from wtforms.validators import DataRequired
 
 import os
 from flask_sqlalchemy import SQLAlchemy
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE']=\
+app.config['SQLALCHEMY_DATABASE_URI']=\
     'sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -37,7 +38,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(64),unique=True,index=True)
-    role_id=db.Column(db.Integer,db.ForeignKey('role.id'))
+    role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
     def __repr__(self):
         return '<User %r>' % self.username
 
