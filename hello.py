c0a3a838287538ab441da270f40cae5129e55f19
gzh <a15839986049@163.com>
2019-07-20 00:46:49 +0800

    使用Flask-Migrate实现数据库迁移

--- hello.py
+++ hello.py
@@ -9,6 +9,8 @@ from wtforms.validators import DataRequired
 import os
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_migrate import Migrate
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
@@ -20,6 +22,7 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+migrate=Migrate(app,db)
 
 class NameForm(FlaskForm):
     name=StringField('What is your name?',validators=[DataRequired()])
@@ -29,7 +32,7 @@ class Role(db.Model):
     __tablename__='roles'
     id = db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(64),unique=True)
-    user=db.relationship('User',backref='role')
+    user=db.relationship('User',backref='role',lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -42,8 +45,10 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-        
-
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+  
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -59,11 +64,17 @@ def index():
     # name=None
     form=NameForm()
     if form.validate_on_submit():
-        old_name=session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user=User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name']=form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html',form=form,name=session.get('name'))
-
-
+    return render_template('index.html',
+        form=form,name=session.get('name'),
+        known=session.get('known',False)) 
