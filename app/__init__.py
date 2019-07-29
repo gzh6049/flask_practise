297260a0cc96b735351267cec00751150e59bd85
gzh <a15839986049@163.com>
2019-07-29 23:34:04 +0800

    添加完成登录表单

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,10 @@ bootstrap = Bootstrap()
 mail=Mail()
 moment=Moment()
 db = SQLAlchemy()
+from flask_login import LoginManager
+
+login_manager=LoginManager()
+login_manager.login_view='auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -20,9 +24,14 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     # 添加路由和自定义的错误界面
+
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/auth')
     return app
