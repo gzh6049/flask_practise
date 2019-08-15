441ead82fa1f1e3e814e042431a97dc0ce5526a4
gzh <a15839986049@163.com>
2019-08-15 23:31:03 +0800

    12345

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-#
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     MAIL_SERVER = os.environ.get('MAIL_SERVER','smtp.googlemail.com')
